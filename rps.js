df3e9a363bfc0529df03c8955d5256eb0de1537d
QuaraAlter <mordreddo@gmail.com>
2022-07-11 22:18:51 +0300

    Add function for player input and visualising inputs of both computer and player

--- rps.js
+++ rps.js
@@ -1,4 +1,7 @@
 var computerPick = '';
+var playerPick = '';
+const button = document.querySelector('button');
+button.addEventListener('click', playerPlay)
 
 function computerPlay()
 {
@@ -6,12 +9,26 @@ function computerPlay()
     switch(n)
     {
         case 0:
-            return 'Rock!'
+            return 'rock'
         case 1:
-            return 'Paper!'
+            return 'paper'
         case 2:
-            return 'Scissors!'
+            return 'scissors'
     }
 }
 
-computerPick = computerPlay();
+function playerPlay ()
+{
+    playerPick = document.getElementById("player").value;
+    if (playerPick.toLowerCase() != `rock` && playerPick.toLowerCase() != `paper` && playerPick.toLowerCase() != `scissors`)   {
+        document.getElementById("error").innerHTML = 'Please pick a valid move';
+        return 0;
+    }
+    document.getElementById("player-pick").innerHTML = playerPick.toLowerCase();
+    computerPick = computerPlay();
+    document.getElementById("computer-pick").innerHTML = computerPick;
+}
+
+
+
+
